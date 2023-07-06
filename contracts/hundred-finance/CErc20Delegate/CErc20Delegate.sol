c4c673de20e611ab85b50994bfe791916c742936
keinberger <philipp.keinberger@gmail.com>
2023-07-06 12:31:54 +0200

    fix: updated compound contracts to be compatible with 0.8 solidity

--- contracts/hundred-finance/CErc20Delegate/CErc20Delegate.sol
+++ contracts/hundred-finance/CErc20Delegate/CErc20Delegate.sol
@@ -17,7 +17,7 @@ contract CErc20Delegate is CErc20, CDelegateInterface {
      * @notice Called by the delegator on a delegate to initialize it for duty
      * @param data The encoded bytes data for any initialization
      */
-    function _becomeImplementation(bytes memory data) public {
+    function _becomeImplementation(bytes memory data) public override {
         // Shh -- currently unused
         data;
 
@@ -32,7 +32,7 @@ contract CErc20Delegate is CErc20, CDelegateInterface {
     /**
      * @notice Called by the delegator on a delegate to forfeit its responsibility
      */
-    function _resignImplementation() public {
+    function _resignImplementation() public override {
         // Shh -- we don't ever want this hook to be marked pure
         if (false) {
             implementation = address(0);
