c818f845c5623fd6f3b238c117f9647023d45123
keinberger <philipp.keinberger@gmail.com>
2023-07-07 13:37:36 +0200

    feat: added foundry unit tests for firewall

--- contracts/firewall/TurtleShellFirewall.sol
+++ contracts/firewall/TurtleShellFirewall.sol
@@ -1,15 +1,25 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
-/// @title TurtleShellFirewall - Firewall implementation, tracks Total Value Locked (TVL) for protocols
-/// @notice This contract allows protocols to track their TVL and set security parameters.
-/// Protocol can increase or decrease their TVL and set a threshold and a number of blocks as security parameters.
-/// Protocol can then check the firewall status to check if its TVL has decreased more than its set threshold since a
-/// set number of blocks ago.
+/**
+ * @title TurtleShellFirewall
+ * @notice This contract is the TurtleShell Firewall implementation, which can be used by any contract to implement an
+ on-chain firewall. The firewall can be configured by the contract owner to set a threshold percentage, block interval and
+ start parameter. The firewall works by checking if the parameter for a given user has changed by more than the threshold, when
+ updating it. If the parameter has changed by more than the threshold, the firewall will be activated for the given user. In a
+ sophistaced implementation, the parameter could possible be the result of a mathematical formula, which takes into account vital
+ parameters of a user (protocol) that should not change by more than a certain threshold. The firewall can be manually deactivated
+ and actived by the user (protocol) at any time.
+ */
 contract TurtleShellFirewall {
     /// @notice This error is thrown if the threshold value is greater than 100 (100%)
     error TurtleShellFirewall__InvalidThresholdValue();
+    /// @notice This error is thrown if the block interval is greater than the total number of blocks
+    error TurtleShellFirewall__InvalidBlockInterval();
+    /// @notice This error is thrown if the startParameter is too big to be multiplied by the threshold percentage
+    error TurtleShellFirewall__InvalidConfigValues();
 
+    /// @dev Firewall configuration values for a given user 
     struct FirewallConfig {
         /// @dev threshold for changes as a percentage (represented as an integer)
         uint8 thresholdPercentage;
@@ -17,6 +27,7 @@ contract TurtleShellFirewall {
         uint256 blockInterval;
     }
 
+    /// @dev Dynamic firewall state data for a given user
     struct FirewallData {
         mapping(uint256 => uint256) parameters;
         bool firewallActive;
@@ -25,18 +36,20 @@ contract TurtleShellFirewall {
     mapping(address => FirewallData) private s_firewallData;
     mapping(address => FirewallConfig) private s_firewallConfig;
 
-    event ParamterChanged(address indexed user, uint256 newParameter);
+    /// @notice Event emitted whenever the parameter for a given user gets changed
+    event ParameterChanged(address indexed user, uint256 indexed newParameter);
+    /// @notice Event emitted whenever the firewall status for a given user gets changed
     event FirewallStatusUpdate(address indexed user, bool indexed newStatus);
 
     /**
      * @notice Function for setting the parameter for a given user
      * @param newParamter The new parameter to set
      * @dev This function is internal and should only be called by the contract itself
-     * This function emits the {ParamterChanged} event
+     * This function emits the {ParameterChanged} event
      */
     function _setParameter(uint256 newParamter) internal {
         s_firewallData[msg.sender].parameters[block.number] = newParamter;
-        emit ParamterChanged(msg.sender, newParamter);
+        emit ParameterChanged(msg.sender, newParamter);
     }
 
     /**
@@ -53,20 +66,17 @@ contract TurtleShellFirewall {
     /**
      * @notice Function for checking if the parameter update is below the threshold
      * @param newParameter The new parameter to check
-     * @return Returns true if the parameter update exceeds the threshold, false otherwise
+     * @return bool true if the parameter update exceeds the threshold, false otherwise
      */
     function _checkIfParameterUpdateExceedsThreshold(uint256 newParameter) internal view returns (bool) {
         FirewallConfig memory m_firewallConfig = s_firewallConfig[msg.sender];
         uint256 referenceParameter =
             s_firewallData[msg.sender].parameters[block.number - m_firewallConfig.blockInterval];
+
+        // insufficient data
+        if (referenceParameter == 0) return false;
+
         uint256 thresholdAmount = (referenceParameter * m_firewallConfig.thresholdPercentage) / 100;
-
-        // console.log("Blocks to reference: %s", m_firewallConfig.blockInterval);
-        // console.log("Threshold percentage: %s", m_firewallConfig.thresholdPercentage);
-        // console.log("Threshold amount: %s", thresholdAmount);
-        // console.log("Reference parameter: %s", referenceParameter);
-        // console.log("New parameter: %s", newParameter);
-
         if (newParameter > referenceParameter) {
             return newParameter - referenceParameter >= thresholdAmount;
         } else {
@@ -75,57 +85,79 @@ contract TurtleShellFirewall {
     }
 
     /**
-     * @notice Function for updating the parameter
+     * @notice Function for updating the security parameter
+     * @dev This function can be called by any user to update their security parameter. If the parameter exceeds the threshold,
+     * the firewall will be automatically activated. If the firewall is already active, the parameter will be updated anyways.
+     * 
+     * Emits the {ParameterChanged} event
+     * Emits the {FirewallStatusUpdate} event
      * @param newParameter is the new parameter
-     * @return Returns true if the parameter update exceeds the threshold (triggers firewall), false otherwise
+     * @return Returns true if the firewall was activated, or had alrady been active
      */
     function updateParameter(uint256 newParameter) external returns (bool) {
         /// @dev gas savings by skipping threshold check in case of active firewall
         if (s_firewallData[msg.sender].firewallActive) {
             _setParameter(newParameter);
-            return false;
+            return true;
         }
 
         bool triggerFirewall = _checkIfParameterUpdateExceedsThreshold(newParameter);
         if (triggerFirewall) _setFirewallStatus(true);
 
-        // console.log("Firewall triggered: %s", triggerFirewall);
-
         _setParameter(newParameter);
         return triggerFirewall;
     }
 
-    /// @notice Function for setting security configuration for the user (protocol)
-    /// @param tresholdPercentage The threshold as a percentage (represented as an integer)
-    function setUserConfig(uint8 tresholdPercentage, uint256 blockInterval, uint256 startParameter) external {
-        if (tresholdPercentage > 100) revert TurtleShellFirewall__InvalidThresholdValue();
-        
-        s_firewallConfig[msg.sender] = FirewallConfig(tresholdPercentage, blockInterval);
+    /**
+     * @notice Function for setting the configuration values for a firewall user
+     * @param thresholdPercentage The threshold percentage to set for the firewall
+     * @param blockInterval The block interval to set for the firewall
+     * @dev The function emits the {ParameterChanged} event
+     */
+    function setUserConfig(uint8 thresholdPercentage, uint256 blockInterval, uint256 startParameter) external {
+        if (thresholdPercentage > 100 || thresholdPercentage == 0) revert TurtleShellFirewall__InvalidThresholdValue();
+        if (blockInterval > block.number) revert TurtleShellFirewall__InvalidBlockInterval();
+        if (startParameter > type(uint256).max / thresholdPercentage) revert TurtleShellFirewall__InvalidConfigValues();
+
+        s_firewallConfig[msg.sender] = FirewallConfig(thresholdPercentage, blockInterval);
         _setParameter(startParameter);
     }
 
+    /**
+     * @notice Function for manually setting the firewall status for a given user
+     * @param newStatus The new status to set for the firewall
+     * @dev This function can be used to manually activate or deactivate the firewall for a given user
+     * ATTENTION: This function should especially be used to deactivate the firewall, in case it got triggered.
+     * This function emits the {FirewallStatusUpdate} event
+     */
     function setFirewallStatus(bool newStatus) external {
         _setFirewallStatus(newStatus);
     }
 
-    /// @notice Check if the firewall is active: the firewall is trigerred when the TVL has decreased more than the set
-    /// threshold since a set number of blocks ago
-    /// @return Returns true if the TVL has decreased more than the threshold, false otherwise
-    function getFirewallStatus() external view returns (bool) {
-        return s_firewallData[msg.sender].firewallActive;
+    /**
+     * @notice Function for getting the firewall status for a given user
+     * @param user The address to get the firewall status for
+     * @return bool if the firewall is active for the given user
+     */
+    function getFirewallStatusOf(address user) external view returns (bool) {
+        return s_firewallData[user].firewallActive;
     }
 
-    /// @notice Get the current paramter for a given user
-    /// @param user The address of the user to get the paramter for
-    /// @return Returns the current paramter of the user
+    /**
+     * @notice Function for getting the security parameter for a given firewall user
+     * @param user The address of the firewall user
+     * @return uint256 the security parameter for the given user
+     */
     function getParameterOf(address user) external view returns (uint256) {
         return s_firewallData[user].parameters[block.number];
     }
 
-    /// @notice Get the security parameters for a given protocol
-    /// @param user The address of the protocol to get the security parameters for
-    /// @return Returns the threshold and number of blocks set as security parameters for the protocol
-    function getSecurityParameterConfigOf(address user) external view returns (uint256, uint256) {
+    /**
+     * @notice Function for getting the security parameters for a given address
+     * @param user The address to get the security parameters for
+     * @return Returns The threshold and block interval set as security parameters for the address
+     */
+    function getSecurityParameterConfigOf(address user) external view returns (uint8, uint256) {
         FirewallConfig memory m_firewallConfig = s_firewallConfig[user];
         return (m_firewallConfig.thresholdPercentage, m_firewallConfig.blockInterval);
     }
--- test/TurtleShellFirewall.t.sol
+++ test/TurtleShellFirewall.t.sol
@@ -12,4 +12,128 @@ contract TurtleShellFirewallTest is Test {
     function setUp() public {
         turtleShellFirewall = new TurtleShellFirewall();
     }
+
+    function testFuzz_setUserConfig_revertsIfThresholdExceeding(uint8 thresholdPercentage) public {
+        vm.assume(thresholdPercentage > 100);
+
+        vm.expectRevert(abi.encodeWithSelector(TurtleShellFirewall.TurtleShellFirewall__InvalidThresholdValue.selector));
+        turtleShellFirewall.setUserConfig(thresholdPercentage, 1, 1);
+    }
+
+    function testFuzz_setUserConfig_revertsIfInvalidBlockInterval(uint256 blockInterval, uint256 blockNumber) public {
+        vm.assume(blockInterval > blockNumber);
+        vm.roll(blockNumber);
+
+        vm.expectRevert(abi.encodeWithSelector(TurtleShellFirewall.TurtleShellFirewall__InvalidBlockInterval.selector));
+        turtleShellFirewall.setUserConfig(5, blockInterval, blockNumber);
+    }
+
+    function testFuzz_setUserConfig_revertsIfInvalidConfigValues(uint256 startParameter, uint8 thresholdPercentage) public {
+        vm.roll(1);
+        vm.assume(thresholdPercentage <= 100 && thresholdPercentage != 0);
+        vm.assume(startParameter > type(uint256).max / thresholdPercentage);
+
+        vm.expectRevert(abi.encodeWithSelector(TurtleShellFirewall.TurtleShellFirewall__InvalidConfigValues.selector));
+        turtleShellFirewall.setUserConfig(thresholdPercentage, 1, startParameter);
+    }
+
+    function testFuzz_setUserConfig_setsConfig(uint256 blockInterval, uint8 thresholdPercentage, uint256 startParameter) public {
+        vm.assume(thresholdPercentage <= 100 && thresholdPercentage != 0);
+        vm.assume(startParameter < type(uint256).max / thresholdPercentage);
+        vm.roll(blockInterval);
+
+        turtleShellFirewall.setUserConfig(thresholdPercentage, blockInterval, startParameter);
+
+        (uint8 retrievedThreshold, uint256 retrievedInterval) =
+            turtleShellFirewall.getSecurityParameterConfigOf(address(this));
+        uint256 retrievedStartParameter = turtleShellFirewall.getParameterOf(address(this));
+
+        assertEq(retrievedThreshold, thresholdPercentage);
+        assertEq(retrievedInterval, blockInterval);
+        assertEq(retrievedStartParameter, startParameter);
+
+        // TODO: test if emits event
+    }
+
+    function testSetFirewallStatus() public {
+        assertEq(turtleShellFirewall.getFirewallStatusOf(address(this)), false);
+
+        turtleShellFirewall.setFirewallStatus(true);
+
+        assertEq(turtleShellFirewall.getFirewallStatusOf(address(this)), true);
+
+        // TODO: test if emits event
+    }
+
+    function testFuzz_updateParameter_setsNewParameter(
+        uint256 blockInterval,
+        uint8 thresholdPercentage,
+        uint256 startParameter,
+        uint256 newParameter
+    )
+        public
+    { 
+        vm.assume(thresholdPercentage <= 100 && thresholdPercentage != 0);
+        vm.assume(startParameter < type(uint256).max / thresholdPercentage);
+        vm.roll(blockInterval);
+
+        turtleShellFirewall.setUserConfig(thresholdPercentage, blockInterval, startParameter);
+
+        turtleShellFirewall.updateParameter(newParameter);
+        assertEq(turtleShellFirewall.getParameterOf(address(this)), newParameter);
+
+        // TODO: check event being emitted
+    }
+
+    function testFuzz_updateParamter_returnsTrueIfFirewallAlreadyActive(
+        uint256 blockInterval,
+        uint8 thresholdPercentage,
+        uint256 startParameter,
+        uint256 newParameter
+    )
+        public
+    {
+        vm.assume(thresholdPercentage <= 100 && thresholdPercentage != 0);
+        vm.assume(startParameter < type(uint256).max / thresholdPercentage);
+        vm.roll(blockInterval);
+
+        turtleShellFirewall.setUserConfig(thresholdPercentage, blockInterval, startParameter);
+        turtleShellFirewall.setFirewallStatus(true);
+
+        assertEq(turtleShellFirewall.updateParameter(newParameter), true);
+        assertEq(turtleShellFirewall.getParameterOf(address(this)), newParameter);
+    }
+
+    function testFuzz_updateParameter_triggersFirewallIfNewParamterExceedsThreshold(
+        uint256 blockInterval,
+        uint8 thresholdPercentage,
+        uint256 startParameter,
+        uint256 newParameter
+    )
+        public
+    {
+        vm.assume(thresholdPercentage <= 100 && thresholdPercentage != 0);
+        vm.assume(startParameter < type(uint256).max / thresholdPercentage);
+
+        vm.assume(startParameter != 0);
+        vm.assume(blockInterval < 20 && blockInterval > 0);
+
+        if (newParameter > startParameter) {
+            vm.assume(newParameter - startParameter > (startParameter * thresholdPercentage / 100));
+        } else {
+            vm.assume(startParameter - newParameter > (startParameter * thresholdPercentage / 100));
+        }
+
+        vm.roll(blockInterval);
+        turtleShellFirewall.setUserConfig(thresholdPercentage, blockInterval, startParameter);
+
+        for (uint256 i = 1; i <= blockInterval; i++) {
+            turtleShellFirewall.updateParameter(startParameter);
+            vm.roll(blockInterval + i);
+        }
+
+        assertEq(turtleShellFirewall.updateParameter(newParameter), true);
+        assertEq(turtleShellFirewall.getFirewallStatusOf(address(this)), true);
+        assertEq(turtleShellFirewall.getParameterOf(address(this)), newParameter);
+    }
 }
